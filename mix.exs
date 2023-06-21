c6e3dbc13f9d4124293bbdb0824f15688d55df3a
Eric Saxby <sax@livinginthepast.org>
2023-06-21 06:52:32 -0700

    Update minimum required version to include Logger.warning

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CpuUtil.MixProject do
     [
       app: :cpu_util,
       version: "0.5.1",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       name: "CpuUtil",
